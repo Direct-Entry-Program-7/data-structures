299ead25b27e4f86c858d38ac554a08fad2c7db2
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-05-06 11:37:15 +0530

    Create class structure

---
+++ Stack/src/ds/Stack.java
@@ -0,0 +1,37 @@
+package ds;
+
+public class Stack {
+
+    public void push(int number){
+
+    }
+
+    public void pop(){
+
+    }
+
+    public boolean empty(){
+        return false;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public int peek(){
+        return 0;
+    }
+
+    public void clear(){
+
+    }
+
+    public void pint(){
+
+    }
+
+    public boolean contains(int number){
+        return false;
+    }
+
+}
