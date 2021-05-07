a55d219860790e59d8993a87d7c232ac3b59cf9a
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-05-07 13:10:24 +0530

    Finalize the Dyamic Array Class

--- DynamicArray/src/ds/DynamicArray.java
+++ DynamicArray/src/ds/DynamicArray.java
@@ -1,37 +1,93 @@
 package ds;
 
+import java.util.Arrays;
+
 public class DynamicArray {
 
-    public void add(int number){}
+    private int[] numbers;
 
-    public void add(int index, int number){
-
+    public void add(int number) {
+        add(size(), number);
     }
 
-    public void get(int index){
+    public void add(int index, int number) {
 
+        if (index > size() || index < 0){
+            throw new RuntimeException("Invalid array index");
+        }
+
+        int[] temp = new int[size() + 1];
+//        for (int i = 0; i < index; i++) {
+//            temp[i] = numbers[i];
+//        }
+//        temp[index] = number;
+//        for (int i = (index+1); i < temp.length; i++) {
+//            temp[i] = numbers[i-1];
+//        }
+        for (int i = 0; i < temp.length; i++) {
+            if (i < index){
+                temp[i] = numbers[i];
+            }else if (i == index){
+                temp[i] = number;
+                continue;
+            }else{
+                temp[i] = numbers[i-1];
+            }
+        }
+        numbers = temp;
     }
 
-    public void remove(int index){
-
+    public int get(int index) {
+        if (index >= size() || index < 0){
+            throw new RuntimeException("Invalid array index");
+        }
+        return numbers[index];
     }
 
-    public void clear(){
-
+    public void remove(int index) {
+        if (index >= size() || index < 0){
+            throw new RuntimeException("Invalid array index");
+        }
+        if (size() -1 == 0){
+            clear();
+            return;
+        }
+        int[] temp = new int[size() - 1];
+        for (int i = 0; i < temp.length; i++) {
+            if (i < index){
+                temp[i] = numbers[i];
+            }else if (i >= index) {
+                temp[i] = numbers[i+1];
+            }
+        }
+        numbers = temp;
     }
 
-    public boolean empty(){
-        return true;
+    public void clear() {
+        numbers = null;
     }
 
-    public int size(){
-        return 0;
+    public boolean empty() {
+        return (numbers == null);
     }
 
-    public boolean contains(){
-        return true;
+    public int size() {
+        return (numbers == null) ? 0 : numbers.length;
     }
-    public void print(){
 
+    public boolean contains(int number) {
+        if (empty()){
+            return false;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == number){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void print() {
+        System.out.println(empty() ? "[]" : Arrays.toString(numbers));
     }
 }
