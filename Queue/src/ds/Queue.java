f1820d7e35fd798bbf578e3b126f2e0020217316
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-05-07 08:42:06 +0530

    Create the basic structure of the Queue class

---
+++ Queue/src/ds/Queue.java
@@ -0,0 +1,37 @@
+package ds;
+
+public class Queue {
+
+    public void enqueue(int number){
+
+    }
+
+    public void dequeue(){
+
+    }
+
+    public void clear(){
+
+    }
+
+    public void print(){
+
+    }
+
+    public int peek(){
+        return -1;
+    }
+
+    public int size(){
+        return -1;
+    }
+
+    public boolean empty(){
+        return true;
+    }
+
+    public boolean contains(int number){
+        return true;
+    }
+
+}
